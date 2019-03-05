9ba6ae034b5b61055d79af1fc36266e136915a1c
faithege <faithege@hotmail.co.uk>
2019-03-05 19:06:45 +0000

    Update UserStory_SQLScripts.sql

--- UserStory_SQLScripts.sql
+++ UserStory_SQLScripts.sql
@@ -188,86 +188,86 @@ WHERE LibraryCardID = ANY (SELECT FROM Loan  WHERE LoanQuantity = <=5);
 
 #### !!!!!!!!!!!!!! NOT READY YET - NEEDS FIXING and few tweaks to db
 
-SELECT B.BookID #Have written Book ID assuming we create a Copy table (BookID primary key, ISBN foreign key inbooks)
-		,B.Title
-		,concat (A.FirstName, ' ', A.SecondName) as 'Author' #may need changing depending on author table
+	SELECT B.BookID #Have written Book ID assuming we create a Copy table (BookID primary key, ISBN foreign key inbooks)
+			,B.Title
+			,concat (A.FirstName, ' ', A.SecondName) as 'Author' #may need changing depending on author table
 
-From Author_Book as AB
-INNER JOIN Book as B
-	ON AB.ISBN=B.ISBN
-INNER JOIN Author as B
-	ON AB.AuthorID=A.AuthorID
-INNER JOIN Loan as L
-	ON L.BookID=B.BookID
-	AND L.DateReturned IS NULL #we want it to join to currently on loan books only, not any historical loans
-#Need to check what this join looks like if a book has multiple authors, do we still get distinct rows? tweak as appropriate
+	From Author_Book as AB
+	INNER JOIN Book as B
+		ON AB.ISBN=B.ISBN
+	INNER JOIN Author as B
+		ON AB.AuthorID=A.AuthorID
+	INNER JOIN Loan as L
+		ON L.BookID=B.BookID
+		AND L.DateReturned IS NULL #we want it to join to currently on loan books only, not any historical loans
+	#Need to check what this join looks like if a book has multiple authors, do we still get distinct rows? tweak as appropriate
 
 
-WHERE GETDATE() > DATEADD(day,30,DateOut) #this function adds 30 days to the date in the DateOut column
-;
+	WHERE GETDATE() > DATEADD(day,30,DateOut) #this function adds 30 days to the date in the DateOut column
+	;
 
 
 
 #UserStory17 - get contact details of a library card holder
 
-SELECT LCH.FirstName
-		,LCH.SecondName
-		,LCH.ContactNumber
-		,COALESCE(LCH.Email,'No email provided')
-		,A.AddressNumber
-		,R.RoadName
-		,C.CityName
-		,P.Postcode
+	SELECT LCH.FirstName
+			,LCH.SecondName
+			,LCH.ContactNumber
+			,COALESCE(LCH.Email,'No email provided')
+			,A.AddressNumber
+			,R.RoadName
+			,C.CityName
+			,P.Postcode
 
-#joining all the address tables
-FROM LibraryCardHolder as LCH
-INNER JOIN Address as A
-ON LCH.AddressID = A.AddressID
+	#joining all the address tables
+	FROM LibraryCardHolder as LCH
+	INNER JOIN Address as A
+	ON LCH.AddressID = A.AddressID
 
-INNER JOIN Road as R
-ON A.RoadID = R.RoadID
+	INNER JOIN Road as R
+	ON A.RoadID = R.RoadID
 
-INNER JOIN City as C
-ON A.CityID = C.CityID
+	INNER JOIN City as C
+	ON A.CityID = C.CityID
 
-INNER JOIN Postcode as P
-ON A.PostcodeID = P.PostcodeID
+	INNER JOIN Postcode as P
+	ON A.PostcodeID = P.PostcodeID
 
 
-WHERE LibraryCardID = '1' #can be changed as necessary
-;
+	WHERE LibraryCardID = '1' #can be changed as necessary
+	;
 
 
 
 #UserStory18 - see what books are available in our branch
 	
-SELECT  
-		B.BookISBN
-		,B.Title 
-		,concat(A.FirstName,' ', A.LastName) as 'Author'
-		,B.YearPublished
-        ,LB.LibraryBranch
-		,Count(DISTINCT C.BookID) as 'CopiesAvailable' #Distinct needed to prevent counting the samebook twice
+	SELECT  
+			B.BookISBN
+			,B.Title 
+			,concat(A.FirstName,' ', A.LastName) as 'Author'
+			,B.YearPublished
+	        ,LB.LibraryBranch
+			,Count(DISTINCT C.BookID) as 'CopiesAvailable' #Distinct needed to prevent counting the same book twice (in case that a book has been returned multiple times)
 
 
-FROM Book AS B
-INNER JOIN bookisbn_authorid as AB
-	ON AB.BookISBN=B.BookISBN
-INNER JOIN Author AS A
-	ON AB.AuthorID = A.AuthorID
-INNER JOIN copy as C
-	ON AB.BookISBN = C.BookISBN
-INNER JOIN librarybranch as LB 
-	ON LB.BranchID = C.BranchID
-INNER JOIN Loan as L 
-	ON C.BookID=L.BookID
+	FROM Book AS B
+	INNER JOIN bookisbn_authorid as AB
+		ON AB.BookISBN=B.BookISBN
+	INNER JOIN Author AS A
+		ON AB.AuthorID = A.AuthorID
+	INNER JOIN copy as C
+		ON AB.BookISBN = C.BookISBN
+	INNER JOIN librarybranch as LB 
+		ON LB.BranchID = C.BranchID
+	INNER JOIN Loan as L 
+		ON C.BookID=L.BookID
 
 
-WHERE DateReturned IS NOT NULL #means only pulls back books that are not checked out
-		AND C.BranchID=1 #can be changed
+	WHERE DateReturned IS NOT NULL #means only pulls back books that are not checked out
+			AND C.BranchID=1 #can be changed
 
-GROUP BY B.BookISBN, B.Title,concat(A.FirstName,' ', A.LastName),B.YearPublished, C.BranchID
-#group statement necessary for count function to work
+	GROUP BY B.BookISBN, B.Title,concat(A.FirstName,' ', A.LastName),B.YearPublished, C.BranchID
+	#group statement necessary for count function to work
 
 
 
@@ -275,29 +275,38 @@ GROUP BY B.BookISBN, B.Title,concat(A.FirstName,' ', A.LastName),B.YearPublished
 	#Havent done this yet as we have not yet created a HR table (on could have list)
 
 
-#User story - see what books are available across branches
-		#NB this is very similar to user story 18
 
-SELECT  B.Title 
-		,Author#may need to update based on db structure
-		,B.Year
-		,Br.Branch
-		,Count (DISTINCT `B.BookID`) as 'CopiesAvailable' #Distinct needed to prevent counting the samebook twice
+#User story 20- see what books are available across branches
+		#NB this is very similar to user story 18, have just removed the where filter for branchID so thatnow pulling all branches
 
-FROM Book as B
-RIGHT OUTER JOIN Loan as L
-ON L.BookID=B.BookID
-
-INNER JOIN Branch as Br
-on B.BranchID=Br.BranchID
+	SELECT  
+			B.BookISBN
+			,B.Title 
+			,concat(A.FirstName,' ', A.LastName) as 'Author'
+			,B.YearPublished
+	        ,LB.LibraryBranch
+			,Count(DISTINCT C.BookID) as 'CopiesAvailable' #Distinct needed to prevent counting the same book twice (in case that a book has been returned multiple times)
 
 
-WHERE DateReturned IS NOT NULL #means only pulls back books that are not checked out
-		AND BranchID='Sky'#can be changed
+	FROM Book AS B
+	INNER JOIN bookisbn_authorid as AB
+		ON AB.BookISBN=B.BookISBN
+	INNER JOIN Author AS A
+		ON AB.AuthorID = A.AuthorID
+	INNER JOIN copy as C
+		ON AB.BookISBN = C.BookISBN
+	INNER JOIN librarybranch as LB 
+		ON LB.BranchID = C.BranchID
+	INNER JOIN Loan as L 
+		ON C.BookID=L.BookID
 
-GROUP BY ISBN,Title,Author,Year #group statement necessary for count function to work
 
-;
+	WHERE DateReturned IS NOT NULL #means only pulls back books that are not checked out
+		
+
+	GROUP BY B.BookISBN, B.Title,concat(A.FirstName,' ', A.LastName),B.YearPublished, C.BranchID
+	#group statement necessary for count function to work
+
 
 
 #USER STORY 20
