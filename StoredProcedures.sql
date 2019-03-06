3467e8ef8484e12f733864ca160d42757ac9d256
MelanieLeather <35497730+MelanieLeather@users.noreply.github.com>
2019-03-06 21:17:09 +0000

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -1,22 +1,21 @@
 -- Find User by forename
 
-CREATE PROCEDURE FindUserByForename(IN thisForename varchar(50))
+CREATE PROCEDURE FindUserByFirstName (IN thisForename VARCHAR(50))
 BEGIN
 	SELECT * 
 	FROM LibraryCardHolder
-    WHERE LibraryCardHolder.forename = thisForename;                          
+    WHERE LibraryCardholder.FirstName = thisForename;                           
 END //
 
-CALL FindUserByForename('Elliot');
+CALL FindUserByFirstName('Elliot');
 
 -- exported from MySQL 
 
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `FindUserByForename`(IN thisForename varchar(50))
-BEGIN
-	SELECT * 
-	FROM LibraryCardholder
-    WHERE LibraryCardholder.Forename = thisForename;                          
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FindUserByFirstName` (IN `thisForename` VARCHAR(50))  BEGIN
+  SELECT * 
+  FROM LibraryCardholder
+    WHERE LibraryCardholder.FirstName = thisForename;                          
 END$$
 DELIMITER ;
 
@@ -24,19 +23,19 @@ DELIMITER ;
 
 -- Find book by title
 
-CREATE PROCEDURE FindBookByTitle(IN thisBookTitle varchar(120))
+CREATE PROCEDURE FindBookByTitle (IN thisBookTitle VARCHAR(120))
 BEGIN
 	SELECT concat(Author.FirstName,' ', Author.LastName) as 'Author', Book.*, CopyAvailibility.IsAvailable, LibraryBranch.LibraryBranch
-	FROM bookisbn_authorid
-	INNER JOIN Book
-	ON bookisbn_authorid.BookISBN=Book.BookISBN
-	INNER JOIN Author
-	ON bookisbn_authorid.AuthorID = Author.AuthorID
-	INNER JOIN CopyAvailibility
-	ON bookisbn_authorid.BookISBN = CopyAvailibility.BookISBN
-	INNER JOIN LibraryBranch 
-	ON CopyAvailibility.BranchCode = LibraryBranch.BranchCode
-	WHERE Book.Title= thisBookTitle;                       
+  FROM bookisbn_authorid
+  INNER JOIN Book
+  ON bookisbn_authorid.BookISBN=Book.BookISBN
+  INNER JOIN Author
+  ON bookisbn_authorid.AuthorID = Author.AuthorID
+  INNER JOIN CopyAvailibility
+  ON bookisbn_authorid.BookISBN = CopyAvailibility.BookISBN
+  INNER JOIN LibraryBranch 
+  ON CopyAvailibility.BranchID = LibraryBranch.BranchID
+  WHERE Book.Title= thisBookTitle;                      
 END //
 
 CALL `FindBookByTitle`('Good Omens');
@@ -45,19 +44,18 @@ CALL `FindBookByTitle`('Good Omens');
 -- exported from MySQL 
 
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `FindBookByTitle`(IN thisBookTitle varchar(120))
-BEGIN
-	SELECT concat(Author.FirstName,' ', Author.LastName) as 'Author', Book.*, CopyAvailibility.IsAvailable, LibraryBranch.LibraryBranch
-	FROM bookisbn_authorid
-	INNER JOIN Book
-	ON bookisbn_authorid.BookISBN=Book.BookISBN
-	INNER JOIN Author
-	ON bookisbn_authorid.AuthorID = Author.AuthorID
-	INNER JOIN CopyAvailibility
-	ON bookisbn_authorid.BookISBN = CopyAvailibility.BookISBN
-	INNER JOIN LibraryBranch 
-	ON CopyAvailibility.BranchCode = LibraryBranch.BranchCode
-	WHERE Book.Title= thisBookTitle;                       
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FindBookByTitle` (IN `thisBookTitle` VARCHAR(120))  BEGIN
+  SELECT concat(Author.FirstName,' ', Author.LastName) as 'Author', Book.*, CopyAvailibility.IsAvailable, LibraryBranch.LibraryBranch
+  FROM bookisbn_authorid
+  INNER JOIN Book
+  ON bookisbn_authorid.BookISBN=Book.BookISBN
+  INNER JOIN Author
+  ON bookisbn_authorid.AuthorID = Author.AuthorID
+  INNER JOIN CopyAvailibility
+  ON bookisbn_authorid.BookISBN = CopyAvailibility.BookISBN
+  INNER JOIN LibraryBranch 
+  ON CopyAvailibility.BranchID = LibraryBranch.BranchID
+  WHERE Book.Title= thisBookTitle;                       
 END$$
 DELIMITER ;
 
@@ -83,9 +81,7 @@ CALL updatePassword('TestProcedure','1');
 
 -- exported from MySQL 
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `updatePassword`(IN thisPassword varchar(20)
-  ,IN thisUserID int(10))
-BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `updatePassword` (IN `thisPassword` VARCHAR(20), IN `thisUserID` INT(10))  BEGIN
 
 UPDATE librarycardholder
 SET librarycardholder.Password  = thisPassword
