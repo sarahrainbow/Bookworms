b5aaeffcdf4302bfc1b139ecb8e69b83e60ca3e1
faithege <faithege@hotmail.co.uk>
2019-03-06 21:27:13 +0000

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -94,3 +94,35 @@ WHERE librarycardholder.librarycardid = thisUserID;
 
 END$$
 DELIMITER ;
+
+--look up availability of a book across branches
+CREATE DEFINER=`root`@`localhost` PROCEDURE `searchBookAvailability` (IN `searchTerm` VARCHAR(100))  BEGIN
+
+SELECT  
+			B.BookISBN
+			,B.Title 
+			,concat(A.FirstName,' ', A.LastName) as 'Author'
+			,B.YearPublished
+	        ,LB.LibraryBranch
+			,Count(DISTINCT C.BookID) as 'CopiesAvailable' #Distinct needed to prevent counting the same book twice (in case that a book has been returned multiple times)
+
+
+	FROM Book AS B
+	INNER JOIN bookisbn_authorid as AB
+		ON AB.BookISBN=B.BookISBN
+	INNER JOIN Author AS A
+		ON AB.AuthorID = A.AuthorID
+	INNER JOIN copy as C
+		ON AB.BookISBN = C.BookISBN
+	INNER JOIN librarybranch as LB 
+		ON LB.BranchID = C.BranchID
+	INNER JOIN Loan as L 
+		ON C.BookID=L.BookID
+
+
+	WHERE DateReturned IS NOT NULL AND Title LIKE concat('%',searchTerm,'%')
+		
+
+	GROUP BY B.BookISBN, B.Title,concat(A.FirstName,' ', A.LastName),B.YearPublished, C.BranchID;
+
+END$$
