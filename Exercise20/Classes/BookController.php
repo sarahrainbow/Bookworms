dfbf5416ec224829528025844f26ed5754843624
csatchi <46576259+csatchi@users.noreply.github.com>
2019-03-19 18:10:56 +0000

    Update BookController.php

--- Exercise20/Classes/BookController.php
+++ Exercise20/Classes/BookController.php
@@ -6,31 +6,34 @@ namespace BookController{
     class BookController{
         public $books=[]; //Declares book type i.e. like Pen in notes
         
+        public function getBook(){
+            return $this->books;
+        }
+        
         public function addbook(Book $book){ //Book is considered a type similar to string which is a defined type
-            array_push($this->books, $book); //Arraypush is premade function which adds variables to the end of established array. Argument $books is from the array function defined which needs to be added into, $book is the variable which is the new book tht will be defined 
-        //$this->books, refers to the object instances (instantiation), so that it considers it as an attribute to relate to the book, $book is the variable
+            array_push($this->books, $book);//Arraypush is premade function which adds variables to the end of established array. Argument $books is from the array function defined which needs to be added into, $book is the variable which is the new book tht will be defined 
+            //$this->books, refers to the object instances (instantiation), so that it considers it as an attribute to relate to the book, $book is the variable
+        }
+        
+        public function deleteBook(Book $deletebook){
+            foreach($this->getBook() as $key => $books){ //use of associative array and therefore can use keys help identify book to be removed
+                if($books->getBookID() === $deletebook -> getBookID()){ //if BookID matches the deleted book ID then delete book
+                    unset($this->books[$key]);
+                    echo "Book ID ". $deletebook->getBookID(). " has been removed from library ";
+                }
+            }
         }
         
         public function countCopies($ISBN){ 
-           $copycount = 0;
-            foreach ($this->books as $book){
+           $copycount = 0; //Starting value of 0 
+            foreach ($this->books as $book){ //iterates through library and if bookISBN matches with search then counts the number of instances
                 if ($book->getISBN() === $ISBN){
                     $copycount++;
                 }
             }
             echo $copycount;
-        }   
-    }
-    
-    $mybookcontroller=new BookController();
-    $mybookcontroller->addbook(new Book(1234, 'The Shining', ['Stephen King'], 3894234, 2034, 'horror'));
-    var_dump($mybookcontroller->books);
-    
-    $mybookcontroller->countCopies(3894234);
- 
-    
-    
-}
+        }
+    }		
     
     
     
