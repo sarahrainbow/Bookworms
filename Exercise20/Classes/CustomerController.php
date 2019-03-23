c7eca952a0c923e575e950823bc52fa495e06256
MelanieLeather <35497730+MelanieLeather@users.noreply.github.com>
2019-03-23 08:06:15 +0000

    Update CustomerController.php

--- Exercise20/Classes/CustomerController.php
+++ Exercise20/Classes/CustomerController.php
@@ -6,16 +6,56 @@
 namespace CustomerController{
     require_once ('Customer.php');
     use Customer\Customer;
-    
+       
     
         class CustomerController{
-            public $customers=[]; //Creates an array for all customer objects, set to public for now just for testing purposes. 
-                                    #This should be replaced by database in future, and update function - otherwise array would need storing on server permanently eg in session
-                   
-            public function addCustomer(Customer $customer){
+            
+        public $customers=[]; //Creates an array for all customer objects, set to public for now just for testing purposes. #This should be replaced by database in future, and update function - otherwise array would need storing on server permanently eg in session      
+ 
+        public function getCustomer(){
+            return $this->customers;
+        }
+            
+        public function addCustomer(Customer $customer){
                 array_push($this->customers, $customer);
+                echo "New customer added\n";
             }
+            
+        public function deleteCustomer(Customer $deleteCustomer){
+            foreach($this->getCustomer() as $key => $customers){ 
+                if($customers->getCustomerID() === $deleteCustomer -> getCustomerID()){ //if CustomerID matches the customer you want to delete, delete it
+                    unset($this->customers[$key]);
+                    echo "Customer ID ". $deleteCustomer->getCustomerID(). " has been removed.";
+                }
+            }
+        }  
+        
+}
+        
+    $testCustomer=new Customer(5567, 'Brian', 'James', 'Baker', 'The Cedars', 'brian@test.com', 'Brian40', 'fsdxc', 'User', '2019-02-01');	 
+    
+    $myCustomer=new CustomerController(); //Instantiating object
+    
+    $myCustomer->addCustomer($testCustomer); //add assigned variable to addCustomer method 
+    var_dump($myCustomer->customers); 
+ 
+    $myCustomer->deleteCustomer($testCustomer); //deletes customer
+        
+}      
 
+
+//       mel's notes 
+//        public function editPrivileges(Customer $editPrivilege){
+//        foreach($this->getCustomer() as $key => $customers){ 
+//        if($customers->getCustomerID() === $editPrivilege -> getCustomerID()
+//        
+//            
+//            //search for privileges key and replace value
+//        }
+//        
+//        }
+            
+            //faith's notes
             ###########NEEDS WORK, HOW DELETE OBJECT BASED ON A CERTAIN ELEMENT, this will also be true for 
             #public function removeCustomer(int $customerID){
                 #if(($key = array_search($customerID, $this->customers, TRUE)) !== FALSE) {
@@ -26,15 +66,11 @@ namespace CustomerController{
                     #unset($this->customers[$customer->getCustomerID()]);
                 #}
             #}
-
 #search for object want
                 #then remove from array
                 #array_push($this->customers, $customer); 
             #}
         #}
-
            #public function setPrivileges(Customer $customer){
                 #array_push($this->customers, $customer);
             #} 
-}
-}
