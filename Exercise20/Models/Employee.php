e192049f4c1ea67a37709fc19d36afa0a6b6d505
faithege <faithege@hotmail.co.uk>
2019-03-24 14:22:38 +0000

    Create Employee.php

---
+++ Exercise20/Models/Employee.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Models{
+
+require_once ('Person.php');   
+use Models\Person;
+
+    class Employee extends Person{ //inherits attributes (name, address etc.) from Person Class allowing additional ones (Job Title, ID) to be added
+        private $employeeID; 
+        private $jobtitle;
+        protected $loanlimit=10;#this overwrites general limit of 5
+
+            public function __construct(int $employeeID, string $jobtitle, string $firstname,  string $secondname,string $surname, string $address, string $email, string $username, string $password, string $privileges, string $datejoined){
+                    $this->employeeID = $employeeID;
+                    $this->jobtitle = $jobtitle;
+                    parent::__construct($firstname, $secondname,$surname, $address, $email, $username, $password,$privileges, $datejoined);
+
+            }
+
+            public function getEmployeeID(){
+                return $this->employeeID;
+            }
+
+            public function setEmployeeID($newEmployeeID){
+                $this->employeeID=$newEmployeeID;
+            }
+
+            public function getJobTitle(){
+                return $this->jobtitle;
+            }
+
+            public function setJobTitle($newJobTitle){
+                $this->jobtitle=$newJobTitle;
+    }
+
+
+    }   
+     
+
+            
+    
+    
+    
+    
+    
+    
+}
