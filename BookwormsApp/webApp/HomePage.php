f13aeb5443e578a311d91a5bcbaf1dcd6ccfba95
MelanieLeather <melanie_leather@hotmail.com>
2019-03-25 21:40:15 +0000

    tidy up of search file extension

--- BookwormsApp/webApp/HomePage.php
+++ BookwormsApp/webApp/HomePage.php
@@ -2,35 +2,33 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Homepage</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="CSS.css" rel="stylesheet" type="text/css"/>
-    <?php
-     
-    ?>
+ 
     </head>
     <body>
         <center>
             <h1>The Bookkeepers</h1>
                 <nav role="navigation">
                     <ul>
-                        <li><a href="">Search, Renew and Reserve books</a>
+                        <li><a href="#">Search, Renew and Reserve books</a>
                             <ul>
                                 <li><a href="">Search by author</a></li>
                                 <li><a href="">Search by title</a></li>
                             </ul>
                         </li>
-                        <li><a href="">Join a Library</a>
+                        <li><a href="#">Join a Library</a>
                             <ul>
-                                <li><a href="">Online application form</a></li>
-                                <li><a href="">Local Branches</a></li>
+                                <li><a href="#">Online application form</a></li>
+                                <li><a href="#">Local Branches</a></li>
                             </ul>
                         </li>
                         <li><a href="">Recommended Reads</a>
                             <ul>
                                 <li><a href="https://www.amazon.co.uk/Best-Sellers-Books/zgbs/books">Bestsellers</a></li>
-                                <li><a href="">Reader's Digest</a></li>
+                                <li><a href="#">Reader's Digest</a></li>
                             </ul>
                         </li>
                     </ul>
