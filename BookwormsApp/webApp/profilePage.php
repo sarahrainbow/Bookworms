0a885550c9c58e73c0fd371eaa67e8f623ebd8ea
faithege <faithege@hotmail.co.uk>
2019-04-02 00:17:57 +0100

    Update profilePage.php

--- BookwormsApp/webApp/profilePage.php
+++ BookwormsApp/webApp/profilePage.php
@@ -22,18 +22,18 @@ and open the template in the editor.
                 <center>
             <h1>Bookworm Libraries</h1>
                         <?php include 'NavBar.html'; ?>
-                </center>            
-        <br>
-        <br>
-        <br><h2>Profile Page</h2>
-        <p>
-            <br>
-            <?php
-            echo '<img src="'.$_SESSION['userDetails']['avatarLocation'].'" alt="Your avatar" style="width:200px;height:200px;"><br>';
-            echo "Hello " . $_SESSION['userDetails']['username'] . '<br>';
-            echo "Your new library card ID is " . $_SESSION['userDetails']['customerID'] .'<br>';
-            ?>
-        </p>
+                </center>
+    
+        <div class="paddedBlock"><h2>Profile Page</h2>
+            <p>
+                <br>
+                <?php
+                echo '<img src="'.$_SESSION['userDetails']['avatarLocation'].'" alt="Your avatar" style="width:200px;height:200px;"><br>';
+                echo "Hello " . $_SESSION['userDetails']['username'] . '<br>';
+                echo "Your new library card ID is " . $_SESSION['userDetails']['customerID'] .'<br>';
+                ?>
+            </p>
+        </div>
 <?php include 'Footer.html';?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
