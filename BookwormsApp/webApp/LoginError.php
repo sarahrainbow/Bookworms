8b9ad58d80a56b44dc8c4f6de63a38221e4dd559
MelanieLeather <melanie_leather@hotmail.com>
2019-04-01 22:13:39 +0100

    adding bootstrap/nav and footer includes file to some pages

--- BookwormsApp/webApp/LoginError.php
+++ BookwormsApp/webApp/LoginError.php
@@ -2,52 +2,18 @@
     <head>
         <meta charset="UTF-8">
         <title>Error Page</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="CSS.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
                 <center>
             <h1>The Bookworms</h1>
-                <nav role="navigation">
-                    <div class = row>
-                    <div class="btn">
-                        <a href="HomePage.php"<button class="btn" type="button" id="option1" data-toggle="dropdown" href="HomePage.php">Homepage</a>
-                            <span class="caret"></span></button>
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="option1" data-toggle="dropdown">Search, Loan and Return Books
-                            <span class="caret"></span></button>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="search.php">Search</a></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="LoanOut.php">Loan</a></li>
-                              <li role="presentation"><a role="menuitem" tabindex="-1" href="LoanReturn.php">Return</a></li>
-                            </ul>                    
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="option1" data-toggle="dropdown">Join a Library
-                            <span class="caret"></span></button>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="customerSignUp.php">Online Application form</a></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Library Branches</a></li>
-                              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Opening times</a></li>
-                            </ul>
-                            
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="option1" data-toggle="dropdown">Recommended Reads
-                            <span class="caret"></span></button>
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="https://www.amazon.co.uk/">Bestsellers</a></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Reader's Digest</a></li>
-                              <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Greatest Books of All Time</a></li>
-                            </ul>
-                            
-                    </div>
-                    <div class="btn">
-                        <a href="LoginPage.php"<button class="btn" type="button" id="option1" data-toggle="dropdown">Account Login</a>
-                            <span class="caret"></span></button>
-                    </div>
-                </nav>
-                    
+            
+            
+                 
+   <?php include 'NavBar.html';?> 
+    </center                
         
         <?php    
             echo ($_SESSION['errorMessage']);
@@ -57,7 +23,7 @@
         <button type="button" onclick="window.location.href = 'LoginPage.php';">Log in again</button>
     
             
-    <?php include 'footer.php';?>
+    <?php include 'footer.html';?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
