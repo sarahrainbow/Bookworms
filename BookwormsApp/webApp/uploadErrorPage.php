4c1c5a21dbe87c4d650e06822ebc1e64acaff7c4
faithege <faithege@hotmail.co.uk>
2019-04-02 00:17:59 +0100

    Update uploadErrorPage.php

--- BookwormsApp/webApp/uploadErrorPage.php
+++ BookwormsApp/webApp/uploadErrorPage.php
@@ -17,18 +17,17 @@
             <h1>Bookworm Libraries</h1>
                 <?php include 'NavBar.html'; ?>
         </center>
-        <br>
-        <br>
-        <br>
-        <h2>Looks like something went a little wrong here...</h2>
-        <p>
-            <?php
-            
-             echo ($_SESSION['uploadError']['errorMessage']);
-             unset($_SESSION['uploadError']['errorMessage']);
-            ?>
-        </p>
-        <button type="button" onclick="window.location.href = 'customerSignUp.php';">I want to try again</button>
+        <div class="paddedBlock"><h2>Looks like something went a little wrong here...</h2>
+        
+            <p>
+                <?php
+
+                 echo ($_SESSION['uploadError']['errorMessage']);
+                 unset($_SESSION['uploadError']['errorMessage']);
+                ?>
+            </p>
+            <button type="button" onclick="window.location.href = 'customerSignUp.php';">I want to try again</button>
+        </div>
 <?php include 'Footer.html';?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
