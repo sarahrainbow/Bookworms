59666eb98c654bbe9c080c7d59eb5d3d67670ffc
MelanieLeather <35497730+MelanieLeather@users.noreply.github.com>
2019-04-03 11:28:03 +0100

    image changes and bootstrap change

--- BookwormsApp/webApp/HomePage.php
+++ BookwormsApp/webApp/HomePage.php
@@ -4,9 +4,7 @@
         <meta charset="UTF-8">
         <title>Home page | Bookworm Libraries</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+ 
 <link href="CSS.css" rel="stylesheet" type="text/css"/>
       
     </head>
@@ -20,7 +18,7 @@
 <div class="row">
 <div class="col-md-4 mt-4">
   <div class="card">
-    <img class="card-img-top" src="Images/JoinLibrary.jpeg" alt="Library">
+    <img class="card-img-top" src="Images/JoinLibrary1.jpg" alt="Library">
     <div class="card-body text-center">
       <a href="customerSignUp.php" class="stretched-link card-title">Join a Library </a>
      </div>
@@ -29,7 +27,7 @@
 
 <div class="col-md-4 mt-4">
   <div class="card">
-    <img class="card-img-top" src="Images/Searchbooks.jpeg" alt="Woman searching for books">
+    <img class="card-img-top" src="Images/library.jpg" alt="Woman reading a book">
     <div class="card-body  text-center">
       <a href="search.php" class="stretched-link card-title">Search books </a>
      </div>
@@ -60,7 +58,7 @@
                             
     <div class="col-md-4 mt-4">
     <div class="card">
-    <img class="card-img-top" src="Images/events.jpeg" alt="Events and Activities">
+    <img class="card-img-top" src="Images/events1.jpg" alt="Events and Activities">
     <div class="card-body  text-center">
     <a href="#" class="stretched-link card-title">Events and Activities </a>
     </div>
@@ -69,7 +67,7 @@
 
 <div class="col-md-4 mt-4">
         <div class="card">
-    <img class="card-img-top" src="Images/onlineservices.jpeg" alt="Online Services">
+    <img class="card-img-top" src="Images/onlineservices1.jpg" alt="Online Services">
     <div class="card-body  text-center">
       <a href="#" class="stretched-link card-title">Online Services </a>
     </div>
@@ -80,9 +78,8 @@
 
   
       <?php include 'Footer.html';?>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 </body>
 </html>
